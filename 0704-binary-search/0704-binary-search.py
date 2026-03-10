2b9e5a4d5f722832e74ab81bd6f56d67ff555e48
Danilo Perić <109467352+ayrtonsenka@users.noreply.github.com>
2026-03-10 20:43:41 +0100

    Time: 3 ms (11.43%), Space: 13.2 MB (92.22%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def search(self, nums, target):
+        n = len(nums)
+        for i in range(n):
+            if nums[i] == target:
+                return i
+
+        return -1
+        
